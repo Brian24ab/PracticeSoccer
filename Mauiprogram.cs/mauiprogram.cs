38d09fc2463f6bbda1d3185b89c8bf6fd1adf6c8
Brian24ab <bmatatwa@gmail.com>
2025-05-05 04:51:24 +0000

    Merge remote changes

--- Mauiprogram.cs/mauiprogram.cs
+++
@@ -1,6 +0,0 @@
-builder.Services.AddSingleton<DrillService>();
-builder.Services.AddSingleton<SessionService>();
-builder.Services.AddSingleton<ProgressViewModel>();
-builder.Services.AddSingleton<DrillDataService>();
-builder.Services.AddSingleton<PracticePlanService>();
-builder.Services.AddSingleton<NotificationService>();
